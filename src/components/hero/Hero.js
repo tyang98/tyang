ebffd9430ed6ddeccadf8b2236d083ba1b318ad1
Tony Yang <txy3@cornell.edu>
2020-03-16 23:15:20 -0400

    upload files to git

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Jumbotron } from "react-bootstrap"
+
+//import custom styles
+import "./hero.css"
+
+const Hero = () => (
+  <Jumbotron style={jumbo_styles} className="jumbo" fluid>
+    <div className="jumbo_content">
+      <h1>Hi, I'm Tony</h1>
+      <br />
+      <h3>
+        I'm currently a CS student at Cornell University, interested in mobile development, computer vision, and systems
+      </h3>
+      <p>Have questions for me?</p>
+      <p>
+        Contact me @ {" "}
+        <a href="mailto:txy3@cornell.edu">
+          <span style={{ color: "blue" }}>txy3@cornell.edu</span>
+        </a>
+      </p>
+    </div>
+  </Jumbotron>
+)
+
+const jumbo_styles = {
+  backgroundColor: "#fff",
+  paddingLeft: "1rem",
+}
+
+export default Hero
